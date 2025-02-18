51799e69bc9e13c1dde7950d9dada3e2a06f43c6
Seth <sethkanoksikarin@gmail.com>
2025-02-18 23:16:07 +0700

    Fixed Flask host binding issue

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=10000)
+
